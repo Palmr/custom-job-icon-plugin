6ccacae5873f5967f42fef75ffcc5c3ff136421f
Nick Palmer <nick@palmr.co.uk>
2015-05-21 14:35:30 +0100

    Updated resize method to interpolate nicely to reduce artifacting

--- src/main/java/jenkins/plugins/jobicon/ImageUtils.java
+++ src/main/java/jenkins/plugins/jobicon/ImageUtils.java
@@ -15,38 +15,49 @@
  */
 package jenkins.plugins.jobicon;
 
-import java.awt.Graphics2D;
+import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import javax.imageio.ImageIO;
 
 /**
  * Utility functions for images.
- * 
- * @author Jean-Christophe Sirot
+ *
+ * @author Jean-Christophe Sirot, Nick Palmer
  */
 class ImageUtils
 {
 	private ImageUtils()
 	{
 	}
-	
+
 	/**
-	 * Resizes the image to 64x64 pixels and convert to PNG.
-	 * @param in the original image
-	 * @param out the converted image
-	 * @param size the image size
-	 * @throws IOException on I/O error
+	 * Resize the image from an InputStream to a specific height/width size
+	 *
+	 * @param in Image InputStream
+	 * @param out OutputStream to write the resized image to
+	 * @param size size of the output image height/width in pixels
+	 * @throws IOException Thrown in the image cannot be read/written to the in/out streams
 	 */
-	static void resize(InputStream in, OutputStream out, int size) throws IOException
-	{
+	static void resize(InputStream in, OutputStream out, int size)
+		throws IOException {
 		BufferedImage originalImage = ImageIO.read(in);
 		BufferedImage resizedImage = new BufferedImage(size, size, BufferedImage.TYPE_INT_ARGB);
+
 		Graphics2D g = resizedImage.createGraphics();
+
+		g.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+		g.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		g.setComposite(AlphaComposite.Src);
+
 		g.drawImage(originalImage, 0, 0, size, size, null);
+
 		g.dispose();
+
+
 		ImageIO.write(resizedImage, "png", out);
 	}
 }
